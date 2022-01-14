f4192568fb3e97de12b5172b3127318d8a0abbbc
agalkin <alexey.galkin@opencascade.com>
2022-01-14 11:44:06 +0300

    Add project template

---
+++ src/test/java/org/tempo/springEdu/SpringEduApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tempo.springEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
