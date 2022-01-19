5d08eaa27e1d866741de4e5028ac405e70632a55
agalkin <alexey.galkin@opencascade.com>
2022-01-19 21:37:38 +0300

    Fix warnings

--- src/main/java/org/tempo/springEdu/contoller/ProjectController.java
+++ src/main/java/org/tempo/springEdu/contoller/ProjectController.java
@@ -1,13 +1,11 @@
 package org.tempo.springEdu.contoller;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.tempo.springEdu.dto.ProjectDto;
 import org.tempo.springEdu.entity.Project;
-import org.tempo.springEdu.repository.ProjectRepository;
 import org.tempo.springEdu.service.ProjectService;
 
 import java.util.List;
@@ -26,9 +24,9 @@ public class ProjectController {
     }
 
     @PostMapping("")
-    public ResponseEntity create(@RequestBody ProjectDto projectDto) {
+    public ResponseEntity<?> create(@RequestBody ProjectDto projectDto) {
         projectService.create(projectDto);
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
@@ -47,25 +45,25 @@ public class ProjectController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity delete(@PathVariable(value = "id") String id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") String id) {
         Optional<Project> delProject = projectService.findById(id);
         if (delProject.isEmpty()) {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             projectService.delete(delProject.get());
-            return new ResponseEntity(HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity update(
+    public ResponseEntity<?> update(
             @PathVariable(value = "id") String id, @RequestBody ProjectDto projectDto) {
         Optional<Project> optionalProject = projectService.findById(id);
         if (optionalProject.isEmpty()) {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
             projectService.update(id, projectDto);
-            return new ResponseEntity(HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
     }
 }
