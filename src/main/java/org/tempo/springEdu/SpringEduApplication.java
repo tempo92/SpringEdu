2054e1a3134978c0df0219efc503f505c5588641
agalkin <alexey.galkin@opencascade.com>
2022-01-21 15:48:19 +0300

    Add local config

--- src/main/java/org/tempo/springEdu/SpringEduApplication.java
+++ src/main/java/org/tempo/springEdu/SpringEduApplication.java
@@ -2,8 +2,14 @@ package org.tempo.springEdu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+    @PropertySource("classpath:application.properties"),
+    @PropertySource(value = "classpath:application.local.properties", ignoreResourceNotFound = true)
+})
 public class SpringEduApplication {
 
     public static void main(String[] args) {
